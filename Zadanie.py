56263fd84da9f3fd97781acc9086c96e2c1a9f6d
MarcinKrze <73644884+MarcinKrze@users.noreply.github.com>
2020-11-18 11:31:48 +0100

    Create Zadanie.py

---
+++ Zadanie.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Przedmiot: Informatyka
+# Kierunek studiów: Mechanika i budowa Maszyn
+# Semestr: zimowy
+# Rok akademicki: 2020/2021
+# Data (dzień.miesiąc.rok): 1.11.2020
+# Imię: Marcin
+# Nazwisko: Krzewiniak
+# Numer albumu ZUT: 49566
